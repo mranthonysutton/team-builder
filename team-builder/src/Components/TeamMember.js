cadd4808a08b4064e89ec64120507736702ae7a7
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-16 14:01:24 -0700

    Styled TeamMember cards

--- team-builder/src/Components/TeamMember.js
+++ team-builder/src/Components/TeamMember.js
@@ -1,20 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+
+const TeamMemberContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const PersonContainer = styled.div`
+  border: 1px solid #ccc;
+  box-shadow: 1px 2px 5px #000;
+  width: 30%;
+  margin: 1.5% 0;
+`;
 
 const TeamMember = props => {
   console.log(props);
   return (
-    <div className="team-member-container">
+    <TeamMemberContainer>
       {/* The props.renderTeam is being passed in from App.js */}
       {props.renderTeam.map((person, index) => {
         return (
-          <div className="team-member" key={index}>
+          <PersonContainer key={index}>
             <h2>{person.fullName}</h2>
             <p>{person.email}</p>
             <p>{person.role}</p>
-          </div>
+          </PersonContainer>
         );
       })}
-    </div>
+    </TeamMemberContainer>
   );
 };
 
