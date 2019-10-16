4696f294b0d241ebed6aa0f25868ed3e8a25a3c7
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-16 12:41:06 -0700

    Added input fields and changeHandler function

--- team-builder/src/Components/UserCreate.js
+++ team-builder/src/Components/UserCreate.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserCreate = props => {
+  const [personData, setPersonData] = useState({
+    fname: "",
+    lname: "",
+    favFood: "",
+    bio: ""
+  });
+
+  const submitForm = event => {
+    event.preventDefault();
+  };
+
+  const changeHandler = event => {
+    // deconstructs person data so that the name and the value of the input fields can be obtained
+    setPersonData({ ...personData, [event.target.name]: event.target.value });
+  };
+
   return (
-    <form>
+    <form onSubmit={submitForm}>
       <label htmlFor="fname">First Name: </label>
-      <input id="fname" name="fname" type="text" placeholder="First Name" />
+      <input
+        id="fname"
+        name="fname"
+        type="text"
+        placeholder="First Name"
+        onChange={changeHandler}
+      />
       <br />
       <label htmlFor="lname">Last Name: </label>
-      <input id="lname" name="lname" type="text" placeholder="Last Name" />
+      <input
+        id="lname"
+        name="lname"
+        type="text"
+        placeholder="Last Name"
+        onChange={changeHandler}
+      />
       <br />
       <label htmlFor="favFood">Favorite Food: </label>
       <input
@@ -15,14 +43,19 @@ const UserCreate = props => {
         name="favFood"
         type="text"
         placeholder="What is your favorite food?"
+        onChange={changeHandler}
       />
       <br />
       <label htmlFor="bio">Bio: </label>
-      <textarea id="bio" name="bio" type="text" placeholder="About You" />
+      <textarea
+        id="bio"
+        name="bio"
+        type="text"
+        placeholder="About You"
+        onChange={changeHandler}
+      />
       <br />
-      <button type="submit" value="Add Team Member">
-        Add Team Member
-      </button>
+      <button type="submit">Add Team Member</button>
     </form>
   );
 };
